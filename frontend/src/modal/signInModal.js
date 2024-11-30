33082299e2a63cb9b827aa962111a220b07de104
zzl-hyun <kimgihyun877@gmail.com>
2024-11-30 16:53:29 +0900

    v0.1.0

--- frontend/src/modal/signInModal.js
+++ frontend/src/modal/signInModal.js
@@ -37,7 +37,8 @@ const SignInModal = ({ isOpen, onClose, onLoginSuccess }) => {
       return;
     }
     try {
-      const response = await axios.post('http://localhost:4000/login', {
+      // const response = await axios.post('http://localhost:4000/login', {
+      const response = await axios.post('http://23.23.207.68:4000/login', {
         id: loginId, // `loginId` 사용
         password: loginPassword, // `loginPassword` 사용
       });
@@ -62,7 +63,8 @@ const SignInModal = ({ isOpen, onClose, onLoginSuccess }) => {
       return;
     }
     try {
-      const response = await axios.post('http://localhost:4000/register', {
+      // const response = await axios.post('http://localhost:4000/register', {
+      const response = await axios.post('http://23.23.207.68:4000/register', {
         id: registerId, // `registerId` 사용
         password: registerPassword, // `registerPassword` 사용
         bio: registerBio || '', // `registerBio` 사용
