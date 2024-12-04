a9a2f7d128e1f6d56c4ce94f0a46b6c2d255abbc
zzl-hyun <kimgihyun877@gmail.com>
2024-12-04 23:38:21 +0900

    Update: ignore div

--- frontend/src/components/home/home-main.js
+++ frontend/src/components/home/home-main.js
@@ -28,8 +28,8 @@ const HomeMain = () => {
     }, []);
 
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
+    // if (loading) return <div>Loading...</div>;
+    // if (error) return <div>Error: {error}</div>;
 
     return (
         <StyledHome>
